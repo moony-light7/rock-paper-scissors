096882cd9655605eb13e025c0c53a44d43e8da83
moony-light7 <muniratopeyemi60@gmail.com>
2025-09-06 11:59:00 +0100

    Add game logic

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ function playGame(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+
+
+function playRound(humanChoice, computerChoice){
+   
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if(humanChoice===computerChoice){
+        console.log("It's a tie! You both chose " + humanChoice);
+    } else if(
+        (humanChoice=== "rock" && computerChoice=== "scissors") ||
+        (humanChoice=== "paper" && computerChoice=== "rock") ||
+        (humanChoice=== "scissors" && computerChoice=== "paper")
+    ){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++
+
+    } else{
+        console.log(`Computer wins! ${computerChoice} beats ${humanChoice}`);
+        computerScore++
+    }
+}
