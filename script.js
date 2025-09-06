d5f05e7623c38c61f357bbe6813be7c2f9a73e08
moony-light7 <muniratopeyemi60@gmail.com>
2025-09-06 11:55:04 +0100

    Add function for computer choice

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3) +1;
+
+    if (choice=== 1){
+        return "Rock";
+    }
+    else if (choice===2){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+}
+
